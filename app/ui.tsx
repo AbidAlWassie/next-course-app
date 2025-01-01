9e0960520037cdfdf620d0f6cab8eaf758a0681b
Abid Al Wassie <abidalwassie@outlook.com>
2025-01-01 19:28:19 +0600

    feat: added backend

---
+++ app/ui.tsx
@@ -0,0 +1,50 @@
+// src/app/ui.tsx
+"use client"
+
+import { Navbar } from "@/components/layouts/Navbar"
+
+// import { Badge } from "@/components/ui/badge"
+// import { MdMessage, MdPeopleAlt } from 'react-icons/md'
+import Link from "next/link"
+import Footer from "../components/layouts/Footer"
+import { Button } from "../components/ui/button"
+import { Card, CardContent } from "../components/ui/card"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-slate-100">
+      <Navbar />
+
+      <div className="max-w-6xl mx-auto py-8 px-4">
+        {status === "loading" && <p className="text-center">Loading...</p>}
+
+        {status === "unauthenticated" && (
+          <div className="text-center">
+            <p className="mb-4">Please sign in to access the chat rooms.</p>
+            <Button
+              asChild
+              className="bg-indigo-600 text-indigo-50 hover:bg-indigo-700"
+            >
+              <Link href="/api/auth/signin">Sign In</Link>
+            </Button>
+            <Footer />
+          </div>
+        )}
+
+        {status === "authenticated" && (
+          <Card className="bg-slate-800 border-slate-700 pt-6 pb-0">
+            <CardContent className="h-[80vh]"></CardContent>
+          </Card>
+        )}
+        <p className="font-bold text-center mt-6">
+          Developed by{" "}
+          <span className="text-indigo-400 hover:text-indigo-500">
+            <Link href="https://abidalwassie.me/" target="_blank">
+              Abid Al Wassie
+            </Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  )
+}
